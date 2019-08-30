085609c1571432e80a4fc24618220143bef20669
João Felipe <jo_5919@hotmail.com>
2019-08-30 20:00:09 -0300

    Fist commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import { Container } from './styles';
+
+export default class Home extends Component {
+  static navigationOptions = {
+    title: 'Home'
+  }
+
+  render() {
+    return (
+      <Container>
+
+      </Container>
+    );
+  }
+}
+
+
---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import Home  from './pages/Home';
+import Cart  from './pages/Cart';
+
+import Header from './components/Header';
+
+import colors from './styles/colors';
+
+const Routes = createAppContainer(createStackNavigator({
+    Home,
+    Cart
+  },
+  {
+    defaultNavigationOptions: navigation => ({
+      header: <Header {...navigation} />
+    }),
+    cardStyle: {
+      backgroundColor: colors.dark,
+    }
+  })
+);
+
+export default Routes;
+
