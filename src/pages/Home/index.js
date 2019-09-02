42918fb81bcf4694b291d2de36f0b8af30d9b110
João Felipe <jo_5919@hotmail.com>
2019-09-02 19:05:52 -0300

    Finish Home style

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Product, ProductImage, ProductTitle,
+  ProductPrice, AddButton, AddButtonText, ProductAmount, ProductAmountText } from './styles';
+
+import api from '../../services/api';
 
 export default class Home extends Component {
-  static navigationOptions = {
-    title: 'Home'
+  state = {
+    products: []
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts = async () => {
+    const response = await api.get('/products');
+
+    this.setState({ products: response.data });
+  }
+
+  renderProduct = ({ item }) => {
+    return (
+      <Product key={item.id}>
+        <ProductImage source={{ uri: item.image }} />
+        <ProductTitle>{item.title}</ProductTitle>
+        <ProductPrice>{item.price}</ProductPrice>
+        <AddButton>
+          <ProductAmount>
+            <Icon name="add-shopping-cart" color="#FFF" size={24}/>
+            <ProductAmountText>0</ProductAmountText>
+          </ProductAmount>
+          <AddButtonText>Adicionar</AddButtonText>
+        </AddButton>
+      </Product>
+    )
   }
 
   render() {
+    const { products } = this.state;
+
     return (
       <Container>
-
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={products}
+          keyExtractor={item => String(item.id)}
+          renderItem={this.renderProduct}
+        />
       </Container>
     );
   }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,70 @@
 import styled from 'styled-components/native';
-import { RectButton } from 'react-native-gesture-handler';
+
+import { darken } from 'polished';
+
+import colors from '../../styles/colors';
 
 export const Container = styled.View`
+  background: ${colors.dark};
+
   flex: 1;
-  padding: 30px;
+  padding: 0;
+  margin-top: 50px;
+`;
+
+export const Product = styled.View`
+  background: #fff;
+  padding: 10px;
+  margin: 15px;
+  border-radius: 4px;
+
+`;
+
+export const ProductImage = styled.Image`
+  height: 250px;
+  width: 250px;
+  align-self: center;
+`;
+
+export const ProductTitle = styled.Text`
+  font-size: 16px;
+`;
+
+export const ProductPrice = styled.Text`
+  margin: 14px 0px;
+  font-size: 20px;
+  margin-bottom: 14px;
+  font-weight: bold;
+`;
+
+export const AddButton = styled.TouchableOpacity`
+  background: ${colors.primary};
+  flex-direction: row;
+  align-items: center;
+  border-radius: 4px;
+  margin-top: auto;
+`;
+
+export const ProductAmount = styled.View`
+  padding: 12px;
+  background: ${darken(0.03, colors.primary)};
+
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProductAmountText = styled.Text`
+  color: #FFF;
+  margin: 0px 4px 0px 10px;
+`;
+
+export const AddButtonText = styled.Text`
+  flex: 1;
+  font-size: 20px;
+  text-align: center;
+  font-weight: bold;
+  color: #FFF;
 `;
